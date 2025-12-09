965f7e40bc09081c281ca193bcae04273a8a5fd3
Sambid Prasad Rijal <sambidrijal71@gmail.com>
2025-12-10 09:14:57 +1000

    New Commit

---
+++ TestProject1/Script/Unit1.js
@@ -0,0 +1,6 @@
+﻿function Test1()
+{
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btEdge).Run("https://www.google.com/");
+  Aliases.browser.BrowserWindow.Maximize();
+}
